48ae53bb84ca57f4d7a01bc33188fc25bbf5f813
Danyang Chen <dychen1999@foxmail.com>
2022-07-14 10:48:58 -0500

    puase

--- Engine.py
+++ Engine.py
@@ -1,6 +1,8 @@
 from Island import Island
 
 from time import sleep
+from sys import stdin 
+from select import select
 
 class Engine():
     def __init__(
@@ -26,7 +28,18 @@ class Engine():
             self.island.trade()
             self.island.reproduce(group_size=10)
             self.island.new_round()
-            # sleep(10)
+
+            if self.island.current_round % self.island._RECORD_PERIOD == 0:
+                self.interupt(1000)
+
+    def interupt(self, wait_time):
+        get_input, _, _ = select([stdin], [], [], 0.1)
+        if get_input and stdin.readline().strip() == "p":
+            print("按回车继续")
+            get_input, _, _ = select([stdin], [], [], wait_time)
+        else:
+            return
+
 
     def save(self):
         pass
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+from sys import stdin 
+from select import select
 
+i, o, e = select([stdin], [], [], 1)
 
-s = input()
+if (i):
+    print("you said", stdin.readline().strip())
+else:
+    print("you said nothing")
