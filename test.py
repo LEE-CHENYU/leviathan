e4d8c1f7beccec5924c678964b199a6a52f608ca
Danyang Chen <dychen1999@foxmail.com>
2022-07-14 10:19:47 -0500

    delete useless file

--- test.py
+++
@@ -1,7 +0,0 @@
-def colored(r, g, b, text):
-    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
-
-text = "red"
-colored_text = colored(0, 200, 0, text)
-
-print(colored_text)
