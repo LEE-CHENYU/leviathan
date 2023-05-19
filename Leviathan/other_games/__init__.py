28100650fbda1d5c309a95a8147ecac1f780171f
Danyang Chen <danyangchen2026@u.northwestern.edu>
2023-05-18 21:04:02 -0500

    two agent game

--- Leviathan/other_games/__init__.py
+++ Leviathan/other_games/__init__.py
@@ -2,9 +2,13 @@ from Leviathan.other_games.two_agent_game import (
     Game,
     GameWHistory,
     GameWHistoryS1Tn,
+    GameWHistorySnD1Tn,
     SBAgent,
     HDAgent,
+    LeviathanAgent,
     suicide_bomber_pay_off,
     RPS_pay_off,
     Hawk_Dove_pay_off,
+    HD_spacial_key_simplifier_1,
+    HD_spacial_key_simplifier_2,
 )
